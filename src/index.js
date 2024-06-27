fb024d2c049b52796d7475dbb34105afcaed1c33
sujatha001122 <sujatha.c2018@vitstudent.ac.in>
2024-06-27 21:55:33 +0530

    Set basename for BrowserRouter and add redirects file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import App from './App';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/blog_app">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
-
 );
